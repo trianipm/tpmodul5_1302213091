5c7cd118b447d242ff3900888a331cec69df00f8
Triani Putri Mumpuni <127008970+trianipm@users.noreply.github.com>
2023-03-19 13:41:15 +0700

    menambahkan class HaloGeneric dengan method  SapaUser

---
+++ Program - Copy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat objek dari class HaloGeneric
+        HaloGeneric halo = new HaloGeneric();
+
+        // Memanggil method SapaUser pada objek halo
+        halo.SapaUser("Triani Putri Mumpuni");
+
+        // Membuat objek dari class DataGeneric
+        DataGeneric<string> data = new DataGeneric<string>("1302213091");
+
+        // Memanggil method PrintData pada objek data
+        data.PrintData();
+
+        Console.ReadLine();
+    }
+}
+
+
